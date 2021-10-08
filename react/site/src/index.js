0b0ec8c159333f6bad8a34fde84660e27d0c065e
vinibessa <vinibessa2005@gmail.com>
2021-10-08 20:09:35 -0300

    teste

--- react/site/src/index.js
+++ react/site/src/index.js
@@ -2,8 +2,13 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import App from '../componentes/src/site/home'
-//coloca os import das telas dps aqui
+import './styles.css'
+
+import App from './InfoX/home'
+import Carrinho from "./InfoX/carrinho"
+import DetalheProduto from "./InfoX/detalheProduto"
+import Contador from "./InfoX/carrinho/contador"
+import CarrinhoItem from "./InfoX/carrinho/carrinhoItem"
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
@@ -13,6 +18,8 @@ ReactDOM.render(
                <Route path="/" exact={true} component={App}/>
                <Route path="/detalhe" component={DetalheProduto}/>
                <Route path="/carrinho"  component={Carrinho}/>
+               <Route path="/contador" exact={true}  component={Contador}/>
+               <Route path="/itemcarrinho"  component={CarrinhoItem}/>
            </Switch>
         </BrowserRouter>
     </StrictMode>,
